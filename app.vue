1c5660618cc2d19530613c98b2405b918d77857c
Yurii Faievskyi <Yurii.Faievskyi@LevelField.us>
2023-01-20 17:25:31 +0200

    add checkbox draft

--- app.vue
+++ app.vue
@@ -1,5 +1,33 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+import CheckBox from "./components/checkbox/checkbox.vue";
+
+export default defineComponent({
+  components: {
+    CheckBox,
+  },
+  data() {
+    return {
+      checked: false,
+      title: "item 1",
+    };
+  },
+  methods: {
+    updateChecked(isChecked: boolean) {
+      this.checked = isChecked;
+    },
+  },
+});
+</script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    <CheckBox
+      :isChecked="checked"
+      :title="title"
+      @updateChecked="updateChecked"
+    />
+    <div>checked: {{ checked }}</div>
   </div>
 </template>
---
+++ components/checkbox/checkbox.vue
@@ -0,0 +1,39 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "CheckBox",
+  props: {
+    isChecked: { type: Boolean, required: true },
+    title: { type: String, required: true },
+  },
+  emits: ["updateChecked"],
+  methods: {
+    onChange(event: Event) {
+      this.checkedState = (event.target as HTMLInputElement).checked;
+
+      this.$emit("updateChecked", this.checkedState);
+    },
+  },
+  data() {
+    return {
+      checkedState: this.isChecked,
+    };
+  },
+});
+</script>
+
+<template>
+  <div>
+    <input
+      type="checkbox"
+      @change="onChange"
+      :id="title"
+      :name="title"
+      :checked="checkedState"
+    />
+    {{ title }}
+  </div>
+</template>
+
+<style></style>
