f519e073eb13e28823020e2ba83ac94d1d6f2379
Yurii Faievskyi <Yurii.Faievskyi@LevelField.us>
2023-01-21 16:13:04 +0200

    add method params,computed,watch

--- components/counter.vue
+++ components/counter.vue
@@ -1,16 +1,36 @@
-<script setup>
-import { reactive } from "vue";
+<script setup lang="ts">
+import { reactive, computed, watch } from "vue";
 
 const simpleTitle = "Not reactive title";
 
 const counter = reactive({ count: 0, counterTitle: "My computer reactive" });
 
-const increment = () => {
-  counter.count++;
+watch(
+  () => counter.count,
+  (newVal, oldVal) => {
+    if (newVal === 20) {
+      counter.counterTitle = "More than 20!!!";
+    } else {
+      counter.counterTitle = "My computer reactive";
+    }
+  }
+);
+
+const evenOrEven = computed(() => {
+  if (counter.count % 2 === 0) {
+    return "even";
+  }
+  return "odd";
+});
+
+const increment = (amount: number, amount2?: number, e?: Event): void => {
+  console.log(e);
+  console.log(amount2);
+  counter.count += amount;
 };
 
-const decrement = () => {
-  counter.count--;
+const decrement = (amount: number): void => {
+  counter.count -= amount;
 };
 </script>
 
@@ -18,9 +38,14 @@ const decrement = () => {
   <div>
     <h1>{{ counter.counterTitle }}</h1>
     <h2>{{ simpleTitle }}</h2>
-    <button @click="increment">+</button>
+    <button @click="increment(2, 3, $event)">+2</button>
+    <button @click="increment(1)">+</button>
     {{ counter.count }}
-    <button @click="decrement">-</button>
+    <button @click="decrement(1)">-</button>
+    <button @click="decrement(2)">-2</button>
+    <div>
+      <p>enen or odd: {{ evenOrEven }}</p>
+    </div>
     <div>
       <input type="text" v-model="counter.counterTitle" />
     </div>
